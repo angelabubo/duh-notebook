ac3bc3e4ed1c68bc2dde29ad1dacf438e83c9587
angel <60677827+angelabubo@users.noreply.github.com>
2020-05-27 17:38:01 -0400

    Initial commit of React CRA practice app

---
+++ build/precache-manifest.4cc4ab701bfbee14d18b9e565ec587dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96893df9467e5f0100fa0876698e847d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d65ec444195668d0922",
+    "url": "/static/js/2.688ceb67.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.688ceb67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b57a3725512cf4a76e",
+    "url": "/static/js/main.bec99551.chunk.js"
+  },
+  {
+    "revision": "6867efb8ce5588a21bce",
+    "url": "/static/js/runtime-main.6580cf22.js"
+  }
+]);
