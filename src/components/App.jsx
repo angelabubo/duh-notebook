47067abae742a35df9c8704b6b692b1c0bf0c021
angel <60677827+angelabubo@users.noreply.github.com>
2020-05-28 12:10:32 -0400

    Updated Note component to use props and populate data using an array of notes in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import Notes from "./../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {Notes.map((noteData) => (
+        <Note
+          key={noteData.key}
+          id={noteData.key}
+          title={noteData.title}
+          content={noteData.content}
+        />
+      ))}
       <Footer />
     </div>
   );
